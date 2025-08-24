9e91736515f29537e0b3ffe8cb921a8d7b36ab83
MehfCodes <mrmohammadehf@gmail.com>
2025-08-24 19:53:21 +0330

    convert Plantitle to String

--- PTM.Contracts/Requests/PlanRequest.cs
+++ PTM.Contracts/Requests/PlanRequest.cs
@@ -1,10 +1,9 @@
 using System;
 
 namespace PTM.Contracts.Requests;
-public enum PlanTitle {Free, premium, Business};
 public class PlanRequest
 {
-    public PlanTitle Title { get; set; } = PlanTitle.Free;
+    public string Title { get; set; } = "Free";
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int MaxTasks { get; set; } = -1;
--- PTM.Contracts/Response/PlanResponse.cs
+++ PTM.Contracts/Response/PlanResponse.cs
@@ -5,7 +5,7 @@ namespace PTM.Contracts.Response;
 public class PlanResponse
 {
     public Guid Id { get; set; }
-    public PlanTitle Title { get; set; } = PlanTitle.Free;
+    public string Title { get; set; } = "Free";
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; } = 0;
     public int MaxTasks { get; set; }
