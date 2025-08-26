f1a2552260b1047e5287c1902c0bc65e473d7091
MehfCodes <mrmohammadehf@gmail.com>
2025-08-27 02:58:52 +0330

    complete controller

--- PTM.API/Controllers/UserController.cs
+++ PTM.API/Controllers/UserController.cs
@@ -20,7 +20,26 @@ namespace PTM.API.Controllers
         public async Task<IActionResult> Add([FromBody] UserRequest request)
         {
             var res = await userService.AddAsync(request);
+            return CreatedAtAction(nameof(Get), new { id = res.Id }, res);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var res = await userService.GetByIdAsync(id);
+            if (res is null) return NotFound("user not found");
             return Ok(res);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await userService.GetAllAsync());
+        
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UserUpdateRequest request)
+        {
+            var res = await userService.UpdateAsync(id, request);
+            if (res is null) return NotFound("not found");
+            return Ok(res);
+        }
+       
     }
 }
