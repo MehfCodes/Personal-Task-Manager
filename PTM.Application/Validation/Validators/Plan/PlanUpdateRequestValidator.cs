a72f56f7c9648a70254d98cd6eef120d980cb520
MehfCodes <mrmohammadehf@gmail.com>
2025-08-27 20:25:00 +0330

    add validators

---
+++ PTM.Application/Validation/Validators/Plan/PlanUpdateRequestValidator.cs
@@ -0,0 +1,13 @@
+using System;
+using FluentValidation;
+using PTM.Contracts.Requests;
+
+namespace PTM.Application.Validation.Validators.Plan;
+
+public class PlanUpdateRequestValidator : AbstractValidator<PlanUpdateRequest>
+{
+    public PlanUpdateRequestValidator()
+    {
+        Include(new PlanUpdateRequestValidator());
+    }
+}
