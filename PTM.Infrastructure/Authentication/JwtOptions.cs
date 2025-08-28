9cd929fa53224f56c0172031d2034a7c87afb0e9
MehfCodes <mrmohammadehf@gmail.com>
2025-08-29 00:23:20 +0330

    install jwt and implement initialsettings

---
+++ PTM.Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PTM.Infrastructure.Authentication;
+
+public class JwtOptions
+{
+    public string SecretKey { get; init; } = string.Empty;
+    public string Issuer { get; init; } = string.Empty;
+    public string Audience { get; init; } = string.Empty;
+    public int AccessTokenDuration { get; init; }
+    public int RefreshTokenDuration { get; init; }
+}
