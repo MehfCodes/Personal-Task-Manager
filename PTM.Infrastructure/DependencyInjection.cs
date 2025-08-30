d141acd2c8c20f4ebc2536dcfb71888c7128edd2
MehfCodes <mrmohammadehf@gmail.com>
2025-08-30 21:36:17 +0330

    add jwt settings

--- PTM.Infrastructure/DependencyInjection.cs
+++ PTM.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using PTM.Application.Interfaces.Repositories;
 using PTM.Infrastructure.Authentication;
 using PTM.Infrastructure.Database;
@@ -23,6 +26,24 @@ public static class DependencyInjection
         // add jwt options
         var JwtOptions = config.GetSection("Jwt");
         services.Configure<JwtOptions>(JwtOptions);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config["Jwt:Issuer"],
+                ValidAudience = config["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:SecretKey"]!)),
+            };
+        });
 
         
         return services;
