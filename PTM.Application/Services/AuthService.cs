b28f51eaba66891051d801b5dc89935c05ded267
MehfCodes <mrmohammadehf@gmail.com>
2025-08-30 21:36:40 +0330

    implement register and login methods

--- PTM.Application/Services/AuthService.cs
+++ PTM.Application/Services/AuthService.cs
@@ -1,22 +1,44 @@
 using System;
+using PTM.Application.Interfaces;
+using PTM.Application.Interfaces.Authentication;
 using PTM.Application.Interfaces.Services;
+using PTM.Application.Mappers;
 using PTM.Contracts.Requests;
 using PTM.Contracts.Response;
+using PTM.Domain.Models;
+using PTM.Infrastructure.Repository;
 
 namespace PTM.Application.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly IUserRepository repository;
+    private readonly ITokenGenerator tokenGenerator;
 
-
-    public Task<UserResponse> Register(UserRegisterRequest request)
-    {
-        throw new NotImplementedException();
+    public AuthService(IUserRepository repository, ITokenGenerator tokenGenerator)
+{
+        this.repository = repository;
+        this.tokenGenerator = tokenGenerator;
     }
 
-    public Task<UserResponse> Login(UserLoginRequest request)
+    public async Task<UserResponse> Register(UserRegisterRequest request)
     {
-        throw new NotImplementedException();
+        var newUser = request.MapToUser();
+        var record = await repository.AddAsync(newUser);
+        var res = record.MapToUserResponse();
+        res.AccessToken = tokenGenerator.CreateAccessToken(newUser);
+        res.RefreshToken = tokenGenerator.CreateRefreshToken().tokenHash;
+        return res;
+    }
+
+    public async Task<UserResponse> Login(UserLoginRequest request)
+    {
+        var user = await repository.GetUserByEmail(request.Email!);
+        if (user is null || BCrypt.Net.BCrypt.Verify(request.Password, user.Password)) throw new UnauthorizedAccessException();
+        var res = user.MapToUserResponse();
+        res.AccessToken = tokenGenerator.CreateAccessToken(user);
+        res.RefreshToken = tokenGenerator.CreateRefreshToken().tokenHash;
+        return res;
     }
 
     public Task<RefreshTokenResponse> RefreshToken(RefreshTokenRequest request)
