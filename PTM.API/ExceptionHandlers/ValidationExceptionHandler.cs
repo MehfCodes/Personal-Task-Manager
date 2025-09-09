42947cbd2cfe04f9bb2ae591a7e442f2a56da059
MehfCodes <mrmohammadehf@gmail.com>
2025-09-10 02:57:05 +0330

    reformat the GetErrors method

--- PTM.API/ExceptionHandlers/ValidationExceptionHandler.cs
+++ PTM.API/ExceptionHandlers/ValidationExceptionHandler.cs
@@ -6,11 +6,17 @@ namespace PTM.API.ExceptionHandlers;
 public class ValidationExceptionHandler : ExceptionHandlerBase<ValidationException>
 {
     protected override int GetStatusCode(ValidationException ex) => StatusCodes.Status400BadRequest;
-        
 
-    protected override string GetMessage(ValidationException ex) => "Validation failed.";
-        
 
-    protected override IDictionary<string, string[]>? GetErrors(ValidationException ex) => ex.Errors;
+    protected override string GetMessage(ValidationException ex) => ex.Message;
+
+
+    protected override IDictionary<string, string[]>? GetErrors(ValidationException ex)
+    {
+        return ex.Errors.GroupBy(e => e.PropertyName).ToDictionary(
+            k => k.Key,
+            v => v.Select(e => e.ErrorMessage).ToArray()
+        );        
+    }
         
 }
