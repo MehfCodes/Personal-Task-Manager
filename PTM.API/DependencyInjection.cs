3bdf03136dd819d98ae17ed3b92c05a16ae7a421
MehfCodes <mrmohammadehf@gmail.com>
2025-09-10 03:24:56 +0330

    repair

--- PTM.API/DependencyInjection.cs
+++ PTM.API/DependencyInjection.cs
@@ -8,10 +8,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddExceptionHandlers(this IServiceCollection services)
     {
-        services.AddScoped<IExceptionHandler, ValidationExceptionHandler>();
-        services.AddScoped<IExceptionHandler, NotFoundExceptionHandler>();
-        services.AddScoped<IExceptionHandler, BusinessRuleExceptionHandler>();
-        services.AddScoped<IExceptionHandler, UnauthorizedExceptionHandler>();
+        services.AddSingleton<IExceptionHandler, ValidationExceptionHandler>();
+        services.AddSingleton<IExceptionHandler, NotFoundExceptionHandler>();
+        services.AddSingleton<IExceptionHandler, BusinessRuleExceptionHandler>();
+        services.AddSingleton<IExceptionHandler, UnauthorizedExceptionHandler>();
 
         services.AddSingleton<IExceptionHandler, InternalExceptionHandler>();
         
