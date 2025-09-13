d3bf55ca0b857103e5068ac46b2026aafe9e3b5c
MehfCodes <mrmohammadehf@gmail.com>
2025-09-13 07:51:06 +0330

    deleted

--- PTM.Contracts/Response/UserPlan/UserPlanDetailResponse.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace PTM.Contracts.Response.UserPlan;
-
-public class UserPlanDetailResponse : UserPlanResponse
-{
-    public UserResponse? User { get; set; }
-    public PlanResponse? Plan { get; set; }
-}
