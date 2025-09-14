a55c99c26a272ecf0d8cd77317cb618456207d98
MehfCodes <mrmohammadehf@gmail.com>
2025-09-14 03:31:15 +0330

    define response dto

---
+++ PTM.Contracts/Response/TaskItem/ChangePriorityResponse.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace PTM.Contracts.Response.TaskItem;
+
+public class ChangePriorityResponse
+{
+    public string? Priority { get; set; }
+}
---
+++ PTM.Contracts/Response/TaskItem/ChangeStatusResponse.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace PTM.Contracts.Response.TaskItem;
+
+public class ChangeStatusResponse
+{
+    public string? Status { get; set; }
+}
