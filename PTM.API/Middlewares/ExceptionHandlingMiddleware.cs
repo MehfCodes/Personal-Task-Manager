e3221da9398a13e84abd80403b653cfbe8db808d
MehfCodes <mrmohammadehf@gmail.com>
2025-09-25 16:39:00 +0330

    add logger

--- PTM.API/Middlewares/ExceptionHandlingMiddleware.cs
+++ PTM.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -7,13 +7,15 @@ namespace PTM.API.Middlewares;
 
 public class ExceptionHandlingMiddleware{
     private readonly RequestDelegate next;
+    private readonly ILogger<ExceptionHandlingMiddleware> logger;
     private readonly IEnumerable<IExceptionHandler> handlers;
     private readonly InternalExceptionHandler internalHandler = new InternalExceptionHandler();
 
-    public ExceptionHandlingMiddleware(RequestDelegate next, IEnumerable<IExceptionHandler> handlers)
+    public ExceptionHandlingMiddleware(RequestDelegate next, IEnumerable<IExceptionHandler> handlers, ILogger<ExceptionHandlingMiddleware> logger)
     {
         this.next = next;
         this.handlers = handlers;
+        this.logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -24,6 +26,7 @@ public class ExceptionHandlingMiddleware{
         }
         catch (Exception ex)
         {
+            logger.LogError(ex, "Exception occurred while processing request {Path}", context.Request.Path);
             var handler = handlers.FirstOrDefault(h => h.CanHandle(ex)) ?? internalHandler;
 
             var response = handler.Handle(ex, context.TraceIdentifier);
