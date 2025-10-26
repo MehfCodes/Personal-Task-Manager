0fac982718a1b08f52b3616766f53d6301764a9b
MehfCodes <mrmohammadehf@gmail.com>
2025-10-26 12:26:10 +0330

    correct speling

--- PTM.Contracts/Requests/TaskItem/BaseTaskItemRequest.cs
+++ PTM.Contracts/Requests/TaskItem/BaseTaskItemRequest.cs
@@ -6,6 +6,6 @@ public class BaseTaskItemRequest
 {
     public required string Title { get; set; }
     public string Description { get; set; } = string.Empty;
-    public string Status { get; set; } = "ToDo";
+    public string Status { get; set; } = "Todo";
     public string Priority { get; set; } = "Mid";
 }
