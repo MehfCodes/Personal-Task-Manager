492086b059dcf0f282532d5a0eb2bec49680ba2a
MehfCodes <mrmohammadehf@gmail.com>
2025-10-31 05:07:59 +0330

    configure proper appsetting.json file for different enviroment

--- PTM.API/Program.cs
+++ PTM.API/Program.cs
@@ -8,7 +8,11 @@ using PTM.Application;
 using PTM.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+       .AddEnvironmentVariables();
 
 
 // Add services to the container.
